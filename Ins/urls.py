2ceaf7c0f945b525f0b11208a57c47f31891289b
HaoYunChen <abab19407@gmail.com>
2020-07-10 14:01:24 -0400

    displayed static page.

--- Ins/urls.py
+++ Ins/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from Ins.views import Helo
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Ins/', include('Ins.urls')),
+    path('', Helo.as_view(), name='home')
 ]
---
+++ Ins/views.py
@@ -0,0 +1,4 @@
+from django.views.generic import TemplateView
+
+class Helo(TemplateView):
+    template_name = 'test.html'
