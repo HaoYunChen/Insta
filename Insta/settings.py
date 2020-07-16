bca26549dc0b7b846740bf1e45192149a0020e54
HaoYunChen <abab19407@gmail.com>
2020-07-16 13:38:54 -0400

    ready for deployment again

--- Insta/settings.py
+++ Insta/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+
 LOGIN_REDIRECT_URL = 'posts'
 LOGOUT_REDIRECT_URL = 'posts'
 
